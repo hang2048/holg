6204f9058b2687153553689479758c795fdf0ee6
HANG <105864471+hang2048@users.noreply.github.com>
2022-12-12 23:30:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,14 +1,14 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: 'HOLG',
+  author: 'HANG',
+  email: 'LZH20022020@OUTLOOK.COM',
+  link: 'https://blog.h-xap.top/',
+  description: 'This Gonna be an AweSome-GUY Website.',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
+  lightBackground: '#f5f5dc', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#0d004d', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
